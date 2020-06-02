ddbbf3b0da60d5c423ee0325c60676409229b3b6
Pasha <9139136@gmail.com>
2020-06-02 18:03:22 +0300

    create model SaleOrder

--- Django_DjangoREST_Vue/orders/models.py
+++ Django_DjangoREST_Vue/orders/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class SalesOrder(models.Model):
+    amount = models.IntegerField()
+    description = models.CharField(max_length=225)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
