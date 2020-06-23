9682ff14ad8502e4e371f8ab38711a6e0490a805
Pasha <9139136@gmail.com>
2020-06-23 18:57:47 +0300

    logo+button

--- Tkinter_lottery_app/Tkinter_lottery_app.py
+++ Tkinter_lottery_app/Tkinter_lottery_app.py
@@ -3,7 +3,29 @@ from random import random, sample
 
 window = Tk()
 
+img = PhotoImage(file='1.gif')
 
+imgLbl = Label(window, image=img)
+label1 = Label(window, relief='groove', width=2)
+label2 = Label(window, relief='groove', width=2)
+label3 = Label(window, relief='groove', width=2)
+label4 = Label(window, relief='groove', width=2)
+label5 = Label(window, relief='groove', width=2)
+label6 = Label(window, relief='groove', width=2)
+getBtn = Button(window)
+resBtn = Button(window)
+
+imgLbl.grid()
+label2.grid()
+label3.grid()
+label4.grid()
+label5.grid()
+label6.grid()
+getBtn.grid()
+resBtn.grid()
+
+window.mainloop()
+exit()
 
 
 num = random()
