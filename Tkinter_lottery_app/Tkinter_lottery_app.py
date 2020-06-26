78ced3ff3e8d52a0682b154cc88b6347c9c27b50
Pasha <9139136@gmail.com>
2020-06-26 16:31:01 +0300

    finish

--- Tkinter_lottery_app/Tkinter_lottery_app.py
+++ Tkinter_lottery_app/Tkinter_lottery_app.py
@@ -2,15 +2,15 @@ from tkinter import *
 from random import random, sample
 
 window = Tk()
-img = PhotoImage(file='1.gif')
+img = PhotoImage(file='1_small.gif')
 
 imgLbl = Label(window, image=img)
-label1 = Label(window, relief='groove', width=2)
-label2 = Label(window, relief='groove', width=2)
-label3 = Label(window, relief='groove', width=2)
-label4 = Label(window, relief='groove', width=2)
-label5 = Label(window, relief='groove', width=2)
-label6 = Label(window, relief='groove', width=2)
+label1 = Label(window, relief='groove', width=4, height=4, font=25, fg='red')
+label2 = Label(window, relief='groove', width=4, height=4, font=25, fg='red')
+label3 = Label(window, relief='groove', width=4, height=4, font=25, fg='red')
+label4 = Label(window, relief='groove', width=4, height=4, font=25, fg='red')
+label5 = Label(window, relief='groove', width=4, height=4, font=25, fg='red')
+label6 = Label(window, relief='groove', width=4, height=4, font=25, fg='red')
 getBtn = Button(window)
 resBtn = Button(window)
 
@@ -26,8 +26,8 @@ resBtn.grid(row=2, column=6, columnspan=2)
 
 window.title('Lotto Number Picker')
 window.resizable(0, 0)
-getBtn.configure(text='Get My Lucky Numbers')
-resBtn.configure(text='Reset')
+getBtn.configure(text='Get My Lucky Numbers', fg='red')
+resBtn.configure(text='Reset', fg='red')
 
 
 label1.configure(text='...')
