9e2410cde9ff17c855f194d9150396791a8d8e6b
Pasha <9139136@gmail.com>
2020-06-26 08:19:42 +0300

    26

--- Tkinter_lottery_app/Tkinter_lottery_app.py
+++ Tkinter_lottery_app/Tkinter_lottery_app.py
@@ -2,9 +2,6 @@ from tkinter import *
 from random import random, sample
 
 window = Tk()
-window.title('Lotto Number Picker')
-window.resizable(0, 0)
-
 img = PhotoImage(file='1.gif')
 
 imgLbl = Label(window, image=img)
@@ -17,16 +14,6 @@ label6 = Label(window, relief='groove', width=2)
 getBtn = Button(window)
 resBtn = Button(window)
 
-getBtn.configure(text='Get My Lucky Numbers')
-resBtn.configure(text='Reset', state=DISABLED)  # Деактивируем кнопку ресет
-
-label1.configure(text='...')
-label2.configure(text='...')
-label3.configure(text='...')
-label4.configure(text='...')
-label5.configure(text='...')
-label6.configure(text='...')
-
 imgLbl.grid(row=1, column=1, rowspan=2)
 label1.grid(row=1, column=2, padx=10)
 label2.grid(row=1, column=3, padx=10)
@@ -37,23 +24,46 @@ label6.grid(row=1, column=7, padx=(10, 20))
 getBtn.grid(row=2, column=2, columnspan=4)
 resBtn.grid(row=2, column=6, columnspan=2)
 
+window.title('Lotto Number Picker')
+window.resizable(0, 0)
+getBtn.configure(text='Get My Lucky Numbers')
+resBtn.configure(text='Reset')
+
+
+label1.configure(text='...')
+label2.configure(text='...')
+label3.configure(text='...')
+label4.configure(text='...')
+label5.configure(text='...')
+label6.configure(text='...')
+
+
+def pick():
+    """Функция генерирует шесть уникальных чисел и присваевает их шести меткам"""
+    nums = sample(range(1, 49), 6)
+    label1.configure(text=nums[0])
+    label2.configure(text=nums[1])
+    label3.configure(text=nums[2])
+    label4.configure(text=nums[3])
+    label5.configure(text=nums[4])
+    label6.configure(text=nums[5])
+    getBtn.configure(text='Get My Lucky Numbers', state=DISABLED)
+    resBtn.configure(text='Reset', state=NORMAL)
 
 
 
+def reset():
+    """Функция выводит многоточие в метках, а также возвращает значение двух кнопок в их изначальное состояние."""
+    label1.configure(text='...')
+    label2.configure(text='...')
+    label3.configure(text='...')
+    label4.configure(text='...')
+    label5.configure(text='...')
+    label6.configure(text='...')
+    getBtn.configure(text='Get My Lucky Numbers', state=NORMAL)
+    resBtn.configure(text='Reset', state=DISABLED)
+
+
+getBtn.configure(command=pick)
+resBtn.configure(command=reset)
 window.mainloop()
-exit()
-
-
-num = random()
-print('Random Float 0.0-1.0: ', num)
-num = int(num * 10)
-print('Random Integer 0-9 :', num)
-nums = []
-i = 0
-while i < 6:
-    nums.append(int(random() * 10) + 1)
-    i += 1
-    print('Random Multiple Integers 1-10 :', nums)
-
-nums = sample(range(1, 49), 6)
-print('Random Integer Sample 1-49 :', nums)
